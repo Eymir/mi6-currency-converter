bd1a03009b7b1dd317e6d277cd628237185eecee
mihai.enea@gmail.com <mihai.enea@gmail.com@0f8e0c01-f80a-5462-f8d0-d494aafca870>
2013-08-29 18:35:55 +0000

    Change style and selector tab

--- trunk/src/com/mi6/currencyconverter/activities/CurrencySelectorActivity.java
+++ trunk/src/com/mi6/currencyconverter/activities/CurrencySelectorActivity.java
@@ -1,14 +1,66 @@
 package com.mi6.currencyconverter.activities;
 
-import com.mi6.currencyconverter.R;
+import java.io.InputStream;
+import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.mi6.currencyconverter.R;
+import com.mi6.currencyconverter.adapters.CurrencyArrayAdapter;
+import com.mi6.currencyconverter.dto.CurrencyDetails;
+import com.mi6.currencyconverter.utils.CurrencyParser;
 
 public class CurrencySelectorActivity extends Activity {
-	
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.currency_selector_layout);
-    }
+    
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		// Set the View layer
+		setContentView(R.layout.selector_listview);
+		setTitle("Select Currencies");
+
+		// Create Parser for raw/countries.xml
+		CurrencyParser currencyParser = new CurrencyParser();
+		InputStream inputStream = getResources().openRawResource(R.raw.currencies);
+		
+		// Parse the inputstream
+		currencyParser.parse(inputStream);
+
+		// Get Countries
+		List<CurrencyDetails> currencyList = currencyParser.getList();
+		
+		
+		// Create a customized ArrayAdapter
+		final CurrencyArrayAdapter adapter = new CurrencyArrayAdapter(
+				getApplicationContext(), R.layout.selector_listitem, currencyList);
+		
+		// Get reference to ListView holder
+		ListView lv = (ListView) this.findViewById(R.id.countryLV);
+		
+		// Set the ListView adapter
+		lv.setAdapter(adapter);
+		
+		lv.setOnItemClickListener(new OnItemClickListener() {
+
+		    public void onItemClick(AdapterView<?> parent, View view,
+		        int position, long id) {
+		       // When clicked, show a toast with the TextView text
+		    	CurrencyDetails currency = (CurrencyDetails) parent.getItemAtPosition(position);
+		    	adapter.remove(currency);
+		       Toast.makeText(getApplicationContext(),
+		         "Clicked on Row: " + currency.getName(), 
+		         Toast.LENGTH_LONG).show();
+		      }
+		     });
+		
+	}
+    
 }
--- trunk/src/com/mi6/currencyconverter/dto/CurrencyDetails.java
+++ trunk/src/com/mi6/currencyconverter/dto/CurrencyDetails.java
@@ -4,8 +4,23 @@ import java.util.List;
 
 public class CurrencyDetails {
 
+	private String code;
 	private String name;
+	private String country;
+	private String flag;
+	
 	private List<RateValues> rateValues;
+	
+	public CurrencyDetails() {
+	}
+	
+	public CurrencyDetails(String code, String name, String country,
+			String flagFilePath) {
+		this.code = code;
+		this.name = name;
+		this.country = country;
+		this.flag = flagFilePath;
+	}
 	
 	public String getName() {
 		return name;
@@ -18,6 +33,24 @@ public class CurrencyDetails {
 	}
 	public void setRateValues(List<RateValues> rateValues) {
 		this.rateValues = rateValues;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getFlag() {
+		return flag;
+	}
+	public void setFlag(String flag) {
+		this.flag = flag;
 	}
 	
 	
